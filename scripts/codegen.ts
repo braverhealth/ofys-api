273ffe7749d48d87f6eef57b3d79b2cd45ebf660
Vincent <vincent@braver.health>
2026-01-08 09:30:46 -0500

    chore: publish package

--- scripts/codegen.ts
+++ scripts/codegen.ts
@@ -4,12 +4,12 @@ import { schema as backendSchema } from '../src/schema/backend.js';
 import { schema as frontendSchema } from '../src/schema/frontend.js';
 import type { OpenAPIV3_1 } from 'openapi-types';
 
-const generate = (filename: string, schema: OpenAPIV3_1.Document) => {
+const generateYamlSchema = (filename: string, schema: OpenAPIV3_1.Document) => {
   const yamlSchema = yaml.dump(schema);
 
   console.log(`Generating ${filename}...`);
   fs.writeFileSync(filename, yamlSchema);
 };
 
-generate('schema/braver-backend.yml', backendSchema);
-generate('schema/braver-frontend.yml', frontendSchema);
+generateYamlSchema('schema/braver-backend.yml', backendSchema);
+generateYamlSchema('schema/braver-frontend.yml', frontendSchema);
---
+++ src/index.ts
@@ -0,0 +1,6 @@
+export { schema as frontendSchema } from './schema/frontend.js';
+export { schema as backendSchema } from './schema/backend.js';
+
+export * from './types/common.js';
+export * as Frontend from './types/frontend.js';
+export * as Backend from './types/backend.js';
